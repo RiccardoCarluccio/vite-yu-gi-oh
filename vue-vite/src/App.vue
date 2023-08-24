bfdec2ff607f1a03db4a47bee0fd262f4bbe58e8
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-08-24 15:58:43 +0200

    make main-container in App.vue

--- vue-vite/src/App.vue
+++ vue-vite/src/App.vue
@@ -9,9 +9,15 @@
 </script>
 
 <template>
-  
+  <TheHeader></TheHeader>
+
+  <div class="main-container">dsada</div>
 </template>
 
 <style lang="scss">
-
+  .main-container {
+    background-color: rgba(212,143,56,255);
+    height: calc(100vh - 100px);
+    padding: 2rem 5rem;
+  }
 </style>
--- vue-vite/src/components/TheHeader.vue
+++ vue-vite/src/components/TheHeader.vue
@@ -5,9 +5,30 @@
 </script>
 
 <template>
-  
+  <div class="main-container">
+    <img src="../assets/yugioh-logo.png" alt="Yu-Gi-Oh Logo">
+    <h1>Yu-Gi-Oh Api</h1>
+  </div>
 </template>
 
 <style scoped lang="scss">
+  .main-container {
+    background-color: white;
+    width: 100%;
+    height: 100px;
+    padding: 1.5rem;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
 
+    img {
+      height: 80px;
+      width: auto;
+    }
+
+    h1 {
+      font-size: 24px;
+      font-weight: 500;
+    }
+  }
 </style>
