c736ef31389c1649370b11d73866872107637768
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-08-26 19:36:42 +0200

    refactor TheMain & TheCardList

---
+++ vue-vite/src/components/TheCardList.vue
@@ -0,0 +1,20 @@
+<script>
+  export default {
+
+  };
+</script>
+
+<template>
+  
+  
+
+</template>
+
+<style scoped lang="scss">
+  .main-content-container {
+    background-color: white;
+    padding: 2rem;
+    width: 100%;
+    height: 100%;
+  }
+</style>
